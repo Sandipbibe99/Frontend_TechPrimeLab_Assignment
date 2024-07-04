1584b90541a850e64fff036626d93406cf0a482a
sandipbibe9912 <sbibe01@gmail.com>
2024-07-05 00:30:10 +0530

    "FE-commit3: clear warnings in console

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -5,7 +5,7 @@ const Login = () => {
   return (
     <div className="flex gap-5 flex-col justify-center overflow-y-hidden items-center min-h-screen  bg-[#f3f5f7] sm:bg-[#ffffff]  bg-contain sm:bg-40%Y   bg-no-repeat overflow-hidden sm:max-h-screen" style={{backgroundImage: `url('/image/login-bg-1.svg')`  }}>
      
-         <img src='/image/Logo.svg' className='mt-16 sm:mt-10 '></img>
+         <img alt='logo' src='/image/Logo.svg' className='mt-16 sm:mt-10 '></img>
          <h5 className='text-white font-extralight'> Online Project Management</h5>
          <LoginCard />
      
